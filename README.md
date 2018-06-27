1d5d971bd973818eed6686d6734c60e0c0aeb9a3
Alessio Santacroce <Alessio.Santacroce@bgch.co.uk>
2018-06-27 12:04:04 +0100

    Initial commit

--- README.md
+++ README.md
@@ -19,13 +19,55 @@ In serverless.yml add the configuration:
 ```
 ...
 provider:
-...
   logsPlugin:
     LogGroup:
-      RetentionInDays: 17  # default 14
-    SubscriptionFilter:
+      RetentionInDays: 14  # optional, default: 90
+    SubscriptionFilter: # optional, default: no SubscriptionFilter resource is added
       DestinationArn: my-destination-arn
-      FilterPattern: my-filtern-pattern  # default '{ $.NotExistingField NOT EXISTS }'
-      RoleArn: my-role-arn  # default null
+      FilterPattern: my-filtern-pattern  # optional, default: '{ $.NotExistingField NOT EXISTS }'
+      RoleArn: my-role-arn  # optional, default: null
+    additionalSubscriptions: # optional, default: no additional SubscriptionFilter
+      - DependsOn: ElasticBeanstalkApp
+        LogGroupNames:
+          - /aws/elasticbeanstalk/myapp/var/log/eb-activity.log
+          - /aws/elasticbeanstalk/myapp/var/log/nginx/access.log
+          - /aws/elasticbeanstalk/myapp/var/log/nginx/error.log
+          - /aws/elasticbeanstalk/myapp/var/log/web-1.error.log
+          - /aws/elasticbeanstalk/myapp/var/log/web-1.log
 ...
 ```
+
+As a result, the plugin will produce, for each lambda function a resource to set the RetentionInDays to the log group and a resource to subscribe the log group:
+```
+    "MyFunctionLogGroup": {
+      "Type": "AWS::Logs::LogGroup",
+      "Properties": {
+        "LogGroupName": "/aws/lambda/MyFunction",
+        "RetentionInDays": 90
+      }
+    },
+    "MyFunctionSubscriptionFilter": {
+      "Type": "AWS::Logs::SubscriptionFilter",
+      "DependsOn": "MyFunctionLambdaFunction",
+      "Properties": {
+        "DestinationArn": "my-destination-arn",
+        "LogGroupName": "/aws/lambda/MyFunction",
+        "FilterPattern": "{ $.NotExistingField NOT EXISTS }"
+      }
+    }  
+```
+
+For each additionalSubscriptions, a subscription resource is produced:
+```
+    "AwselasticbeanstalkmyappvarlogwebDash1logSubscriptionFilter": {
+      "Type": "AWS::Logs::SubscriptionFilter",
+      "DependsOn": "ElasticBeanstalkApp",
+      "Properties": {
+        "DestinationArn": "my-destination-arn",
+        "LogGroupName": "/aws/elasticbeanstalk/myapp/var/log/web-1.log",
+        "FilterPattern": "{ $.NotExistingField NOT EXISTS }"
+      }
+    }
+```
+
+
--- index.js
+++ index.js
@@ -8,45 +8,82 @@ class ServerlessPlugin {
     };
   }
 
-  normalizeFunctionName(name) {
-    name = name.replace('-', 'Dash').replace('_', 'Underscore');
-    return name.charAt(0).toUpperCase() + name.substr(1);
-  }
-
   afterPackageInitialize() {
-    const config = this.serverless.service.provider.logsPlugin || {}
-    // setting defaults for missing config parameter
-    config.LogGroup = config.LogGroup || {};
-    config.LogGroup.RetentionInDays = config.LogGroup.RetentionInDays || 14;
-    config.SubscriptionFilter = config.SubscriptionFilter || {}
-    config.SubscriptionFilter.FilterPattern = config.SubscriptionFilter.FilterPattern || '{ $.NotExistingField NOT EXISTS }'; // filter to accept all logs
+    const config = this.fetchConfig()
 
     const functions = this.serverless.service.functions
     const resources = this.serverless.service.resources.Resources
+    const cli = this.serverless.cli
+
+    const addLogSubscriptionResource = function(resourceName, logGroupName, dependsOn) {
+        if (!config.SubscriptionFilter.DestinationArn) {
+            return;
+        }
+        resources[resourceName] = {
+            'Type' : 'AWS::Logs::SubscriptionFilter',
+            'DependsOn' : dependsOn,
+            'Properties': {
+                'DestinationArn': config.SubscriptionFilter.DestinationArn,
+                'LogGroupName': logGroupName,
+                'FilterPattern': config.SubscriptionFilter.FilterPattern
+            }
+        };
+        if (config.SubscriptionFilter.RoleArn) {
+            resources[resourceName]['Properties']['RoleArn'] = config.SubscriptionFilter.RoleArn
+        }
+        cli.log('Added resource Subscription for log group ' + logGroupName);
+    }
+
     for(var func in functions) {
-        this.serverless.cli.log('Adding Log RetentionInDays ' + config.RetentionInDays +' for function ' + func);
-        const normalizedFunctionName = this.normalizeFunctionName(func)
-        const logGroup = normalizedFunctionName + 'LogGroup'
+        const normalizedFunctionName = this.normalizeResourceName(func)
         const logGroupName = '/aws/lambda/' + functions[func]['name']
-        resources[logGroup] = {
+        resources[normalizedFunctionName + 'LogGroup'] = {
             'Properties': {
                 'LogGroupName': logGroupName,
                 'RetentionInDays': config.LogGroup.RetentionInDays
             }
         }
-        if (config.SubscriptionFilter.DestinationArn) {
-            this.serverless.cli.log('Adding Log Subscription for function ' + func);
-            resources[normalizedFunctionName + 'SubscriptionFilter'] = {
-                'Type' : 'AWS::Logs::SubscriptionFilter',
-                'Properties': {
-                    'DestinationArn': config.SubscriptionFilter.DestinationArn,
-                    'LogGroupName': logGroupName,
-                    'FilterPattern': config.SubscriptionFilter.FilterPattern,
-                    'RoleArn': config.SubscriptionFilter.RoleArn
-                }
-            }
+        cli.log('Added RetentionInDays ' + config.RetentionInDays +' for log group ' + logGroupName);
+
+        const subscriptionResourceName = normalizedFunctionName + 'SubscriptionFilter';
+        addLogSubscriptionResource(subscriptionResourceName, logGroupName, func);
+    }
+
+    for (var i = 0; i < config.additionalSubscriptions.length; i++) {
+        const subscription = config.additionalSubscriptions[i];
+        for (var j = 0; j < subscription.LogGroupNames.length; j++) {
+            const logGroupName = subscription.LogGroupNames[j];
+            const resourceName = this.normalizeResourceName(logGroupName) + 'SubscriptionFilter';
+            cli.log('Adding Subscription for log group ' + logGroupName);
+            addLogSubscriptionResource(resourceName, logGroupName, subscription.DependsOn);
         }
     }
+  }
+
+  fetchConfig() {
+    const config = this.serverless.service.provider.logsPlugin || {}
+    // setting defaults for missing parameters
+    config.LogGroup = config.LogGroup || {};
+    config.LogGroup.RetentionInDays = config.LogGroup.RetentionInDays || 90;
+    config.SubscriptionFilter = config.SubscriptionFilter || {};
+    config.SubscriptionFilter.FilterPattern = config.SubscriptionFilter.FilterPattern || '{ $.NotExistingField NOT EXISTS }'; // filter to accept all logs
+    config.additionalSubscriptions = config.additionalSubscriptions || [];
+    return config;
+  }
+
+  normalizeResourceName(name) {
+    name = this.replaceAll(name, '.', '');
+    name = this.replaceAll(name, '/', '');
+    name = this.replaceAll(name, '-', 'Dash');
+    name = this.replaceAll(name, '_', 'Underscore');
+    return name.charAt(0).toUpperCase() + name.substr(1);
+  }
+
+  replaceAll(str, oldToken, newToken) {
+    while (str.indexOf(oldToken) > -1) {
+        str = str.replace(oldToken, newToken);
+    }
+    return str;
   }
 }
 
